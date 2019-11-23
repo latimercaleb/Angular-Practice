25092c236e4ca017ceeca3fbc29f4ade3564a3d2
latimercaleb <nohitokaze@gmail.com>
2019-11-23 16:37:48 -0500

    Outlining template and ts

--- RxJs/src/app/app.component.ts
+++ RxJs/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+    header: String;
+    subheaderText: String[];
+
+    ngOnInit(){
+      this.header = "RxJs: Basics in Practice"
+      this.subheaderText = [];
+    }
+
+    makeNewText(header: string){
+      const number = ++ this.subheaderText.length; 
+      const newMessage = `Sub ${number}: ${header}`;
+      this.subheaderText.push(newMessage);
+    }
+
+    ngOnDestroy(){
+
+    }
 }
