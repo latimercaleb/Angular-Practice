b4a11a3d5203517cdf33fad942f9470ddf85be9c
latimercaleb <nohitokaze@gmail.com>
2019-11-23 21:32:51 -0500

    Adding custom observable

--- RxJs/src/app/app.component.ts
+++ RxJs/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { throttleTime, map } from 'rxjs/operators';
 
 @Component({
@@ -10,14 +10,30 @@ import { throttleTime, map } from 'rxjs/operators';
 export class AppComponent implements OnInit, OnDestroy {
     header: String;
     subheaderText: String[];
+
     obs1: {cx: number, cy:number};
     obs1clicked: boolean;
 
+    obs2clicked: boolean;
+    obs2Data: String[];
+
+    myObserver: any = {
+      next: (x: number) => this.obs2Data.push(`Input of ${x} stored as ${Math.pow(x,2)}`),
+      error: (x: any) => this.obs2Data.push(`Error of ${x}`),
+      complete: () => this.obs2Data.push(`Complete() on Obs 2 was called`)
+    };
+
+    testObs: any;
+
     ngOnInit(){
       this.header = "RxJs: Basics in Practice"
-      this.subheaderText = [];
+      this.subheaderText = ['Custom Observer'];
       this.obs1 = {cx:0, cy:0};
       this.obs1clicked = false;
+      this.testObs = null;
+      this.obs2clicked = false;
+      this.obs2Data = [];
+      this.makeNewText('RxJS Operators, map() & throttleTime()');
     }
 
     makeNewText(header: string){
@@ -27,7 +43,7 @@ export class AppComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(){
-
+      this.testObs.unsubscribe();
     }
 
     triggerEvent(evtData: any){
@@ -51,6 +67,17 @@ export class AppComponent implements OnInit, OnDestroy {
         (err) => console.log('This would be an error, but there\'s no error on a click evt'),
         () => {console.log('Obs 1 completed');}
       );
+    }
 
+    customObservable(){
+      this.obs2clicked = true;
+      this.testObs = Observable.create(function(obs){
+        obs.next(2);
+        //obs.error('asudhf');    
+        setTimeout(() => {
+          obs.complete();
+        },3000);
+        obs.next(3);
+      }).subscribe(this.myObserver)
     }
 }
