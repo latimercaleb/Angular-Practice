584f40073d67f7b348ce4591037e7570ea635e1b
latimercaleb <nohitokaze@gmail.com>
2019-11-28 00:54:42 -0500

    Starting compiler practice

--- Typescript/Project/app.js
+++ Typescript/Project/app.js
@@ -1,11 +1,3 @@
-var ui;
-var ux;
-ui = 5;
-ui = true;
-ui = 'phrase';
-ux = ui; // This isn't alllowed with unknown without being casted
-// unknown works like any but it's not assignable to other values
-function generateFlaw(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateFlaw('an error!', 3349);
+"use strict";
+var foo = 'tacos';
+console.log(foo);
