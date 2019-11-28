98432b5ba0f0393fd3ac07d1f63b78250fc88189
latimercaleb <nohitokaze@gmail.com>
2019-11-28 03:09:49 -0500

    Compiler options review

---
+++ Typescript/Project/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const foo = 'tacos';
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+console.log(foo);
---
+++ Typescript/Project/src/app.ts
@@ -0,0 +1,7 @@
+const foo = 'tacos';
+const button = document.querySelector('button')!;
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+
+console.log(foo);
