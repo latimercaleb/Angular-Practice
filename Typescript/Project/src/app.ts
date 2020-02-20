5233c6e9b1cf5a2b12a087b1342ad19761c95ec9
latimercaleb <nohitokaze@gmail.com>
2020-02-20 01:06:16 -0500

    Adding furtheer type work + generics

--- Typescript/Project/src/app.ts
+++ Typescript/Project/src/app.ts
@@ -1,127 +1,98 @@
-// Intersection types
-type Admin = {
-    name: string;
-    priviledges: string[];
-};
-
-type Employee = {
-    name: string;
-    startDate: Date;
-};
-
-
-// The same thing can be acheived with interfaces and elevated employee extending like 
-// interface ElevatedEmployee extends Employee, Admin{...}
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-    name: 'Newbie',
-    priviledges: ['push-code'],
-    startDate: new Date()
-};
-// Intersection types can be used with any type
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric; // Gets numeric since it's an intersection
-
-function add(a: Combinable, b:Combinable){
-    if(typeof(a) === 'string' || typeof(b) === 'string' ){ // basic example of type guard
-        return a.toString() + b.toString();
-    }
-    return a +b;
-}
-
-type UnknownEmployee = Employee | Admin;
-
-function printEmployeeInformation(emp: UnknownEmployee){
-    console.log(`Name: ${emp.name}`);
-    // The type guard above won't work need to leverage another way to check properties to define types
-    if('priviledges' in emp){
-        console.log(`Priviledges: ${emp.priviledges}`);
-    }
-    if('startDate' in emp){
-        console.log(`Start Date: ${emp.startDate}`);
-    }
-}
-
-printEmployeeInformation(e1);
-printEmployeeInformation({name: 'Bad Employee', startDate: new Date()});
-
-// Class type guards can use instanceof as a type guard, instanceof doesn't work inside of interfaces though since they don't compile into anything
-class Car{
-    drive(){
-        console.log('Driving');
-    }
-}
-class Truck{
-    drive(){
-        console.log('Truck is driving');
-    }
-    loadCargo(){
-        console.log('Cargo loading');
-    }
-}
-
-type Vehicle = Car | Truck;
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle){
-    vehicle.drive();
-    if (vehicle instanceof Truck){
-        vehicle.loadCargo();
-    }
-}
-
-useVehicle(v1);
-useVehicle(v2);
-
-// Discriminated Unions
-interface Bird {
-    type: 'bird';
-    flyingSpeed: number;
-}
-
-interface Horse {
-    type: 'horse';
-    runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal){
-    // Pass in a type-literal property, then in the method swap on that
-    let speed: number;
-    switch(animal.type){
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-    }
-    console.log(`Moving with speed: ${speed}`);
-}
-
-moveAnimal({type: 'bird', flyingSpeed:25});
-
-// Type Casting
-
-const pTag = document.querySelector('p'); // Returns HTMLParagraphElement | null
-const pTagById = document.getElementById('testId'); // Returns HTMLElement | null, TS can't infer what html element since I didn't fetch it
-const input = document.getElementById('input')!; // Adding the exclamation point means not null
-// input.value = 'Hello Error';  // Renders, but throws a TS err because TS doesn't recognize it as an input
-
-// Method 1- Pre-cast: Include type for property in front with <>, ok for Angular, bad for React
-const castedInput = <HTMLInputElement>document.getElementById('input')!; 
-castedInput.value = 'Hello Success1'; 
-
-// Method 2- Post-cast: Include type for property after with as keyword, good for either or
-const secondCastedInput = document.getElementById('input')! as HTMLInputElement; 
-castedInput.value = 'Hello Success2'; 
-
 // Index Properties
+// The below is nice for errors but what if you wanted it to cover undefined errors and only store errors when they are emitted
+interface ErrContainer {
+    email: 'Not a valid email';
+    username: 'Must start with a valid character';
+}
 
+// Use an index type
+interface ErrorContainer {
+    // Don't know the property name, how many properties or what the value is
+    // But I do know the type
+    [prop: string]: string;
+    // cid: number; // This will trigger error because this is an index type now
+}
 
+const errs: ErrorContainer = {
+    email: 'Not a valid email',
+    username: 'Must start with a valid character',
+};
 
+// function overloading- define multiple function sigs for one function
+// If using more or less params make sure to take them as optional with ?: 
+function add(n: number): number;
+function add(a: number, b?: number): number;
+function add(a: string, b?: string) { 
+    // Code here
+    return a + b;
+}
+
+// Optional Chaining: Say your getting a complex object from an api and you don't know if the prop name is contained
+const fetchedData = {
+    id: 'xG42',
+    name: 'Korn',
+    job: {
+        // title: 'QA',
+        description: 'I do stuff'
+    }
+};
+console.log(fetchedData.job && fetchedData.job.title); // Old school way of checking if it exists
+console.log(fetchedData?.job?.title); // Optional chaining, does the property exist? If so continue chain if not cancel the call
+
+// Nullish Coalescing
+const userInput = null;
+const store = userInput || 'Some Default'; // Gets the default if falsy (null, NAN, 0, empty string, undefined)
+const otherStore = userInput ?? 'Some Default' // accepts blanks as valid values and only works on null or undefined values 
+
+// Generics: Enforce better type safety by getting additional type information
+
+// Arrays are generics and it uses the params passed in to infer it's type
+const names = ['alpha', 'beta'];
+const otherStuff: Array<string> = []; // Declare type with <>
+
+// Can say it will always return a string and typescript can support string methods on the prm 
+const prm: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Completed!');
+    }, 2000);
+});
+
+prm.then(data => data.split(' '));
+
+// The generic function
+function mergeObj(objA: object, objB: object){
+    return Object.assign(objA, objB);
+}
+
+console.log(mergeObj({name: 'alpha'}, {age: 'beta'}));
+const m = mergeObj({name: 'alpha'}, {age: 'beta'});
+m.age; m.name; // This doesn't work since TS doesn't know type, could typecast, but this would be a good case for generics.
+
+function mergeGeneric<T, U>(objA: T, objB: U){ // TS knows that this is the intersection type now
+    // Could pass in concrete types above instead of using generics but it'd be redundant
+    return Object.assign(objA, objB);
+}
+
+const gen = mergeGeneric({name: 'alpha'}, {age: 'beta'});
+gen.age; gen.name;  // This works
+console.log (`${gen.age} of ${gen.name}`);
+
+// Even generics require some sort of type limitation to be effective, these are called type constraints
+function mergeConstrained<T extends object, U extends object>(objA: T, objB: U){ // TS knows that this is the intersection type now
+    // Could pass in concrete types above instead of using generics but it'd be redundant
+    return Object.assign(objA, objB);
+}
+
+const unassignable = mergeConstrained({name: 'foo', age: 21}, 50); // 50 will throw type error as it should this helps avoid errors
+console.log(unassignable);
+interface hasLength {
+    length: number;
+}
+
+function countNPrint<T extends hasLength>(elm:T): [T, string]{
+    let text = 'none';
+    elm.length > 0 ? text = `Length is ${elm.length}` : ''; // Use constraint to enforce that param does indeed have a length property
+    return [elm, text];
+}
+
+console.log(countNPrint(5));
